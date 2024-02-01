c5bcb97b979b79621873d3180a2b9a605c1afc5d
Gucci0147 <emmanueltimileyin171@gmail.com>
2024-02-01 01:03:24 +0000

    first commit

---
+++ microsofts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import RegistrationForm
+
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page or login page
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'index.html', {'form': form})
+
