55353c4b12f5a53f121f9b776f01236a69b98ba1
Gucci0147 <emmanueltimileyin171@gmail.com>
2024-02-02 11:59:25 +0000

    pushing to render

--- mymicrosoft/settings.py
+++ mymicrosoft/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1$%=v@k7de2*0i379*na=3$@5f(50&l5moorna*wnu)zt7l5y!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "microsoft-drive-web.onrender.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'microsoft-drive-web.onrender.com']
 
 
 # Application definition
